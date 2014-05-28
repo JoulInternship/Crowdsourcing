1052b81cc74878c187639622d44828299e647388
Guillaume Wuip <guillaume@wuips.com>
2014-05-28 10:03:33 +0200

    Home page
    List things + some actions

---
+++ js/controllers/listController.js
@@ -0,0 +1,50 @@
+(function () {
+
+    'use strict';
+
+    window.app.controller('listController', [
+        '$scope',
+        '$rootScope',
+        'thingService',
+        '$routeParams',
+        'userService',
+        'thingService',
+        function ($scope, $rootScope, $http, $routeParams, userService, thingService) {
+
+
+            var getThings = function () {
+                $rootScope.loading = true;
+                thingService.get().then(function (result) {
+                    $scope.things = result;
+                    $rootScope.loading = false;
+                });
+            };
+
+            $scope.join = function (url) {
+
+                $rootScope.loading = true;
+
+                thingService.join(url, function () {
+                    $rootScope.loading = false;
+                    getThings();
+                });
+
+            };
+
+            $scope.exit = function (url) {
+
+                $rootScope.loading = true;
+
+                thingService.exit(url, function () {
+                    $rootScope.loading = false;
+                    getThings();
+                });
+
+            };
+
+
+            getThings();
+        }
+    ]);
+
+}());
