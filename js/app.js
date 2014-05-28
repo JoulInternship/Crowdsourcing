5ba9bd3da0fe5c2315273967577d64037deab587
Guillaume Wuip <guillaume@wuips.com>
2014-05-28 09:56:58 +0200

    Add route logic

--- js/app.js
+++ js/app.js
@@ -5,5 +5,23 @@
     window.app = angular.module('crowd', ['ngRoute', 'ngResource']);
 
 
+    window.app.config(function ($routeProvider, $httpProvider) {
+
+        $routeProvider
+            .when('/', {
+                templateUrl: 'partials/home.html',
+                controller: 'listController'
+            })
+            .when('/me', {
+                templateUrl: 'partials/me.html',
+                controller: 'meController'
+            })
+            .otherwise({redirectTo: '/'});
+
+        $httpProvider.defaults.headers.common.Accept = 'application/json, text/javascript';
+
+    });
+
+
 
 }());
